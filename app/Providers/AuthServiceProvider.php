d54eab0026629905c927f096faa3e9a6f50e6339
Everaldo da Costa Filho <everaldo.webmail@gmail.com>
2019-06-19 20:41:27 -0300

    Adicionado Autenticação com a nova tabela usuario

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,10 +29,10 @@ class AuthServiceProvider extends ServiceProvider
         // application. The callback which receives the incoming request instance
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
-
+        
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            if ($request->input('token')) {
+                return User::where('token', $request->input('token'))->first();
             }
         });
     }
